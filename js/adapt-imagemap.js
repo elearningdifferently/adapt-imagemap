af83d37529d117134febfb7c27adea87f61354fc
Stephen Harlow <stephen.harlow@gmail.com>
2018-09-26 10:58:51 +1200

    Update adapt-imagemap.js

--- js/adapt-imagemap.js
+++ js/adapt-imagemap.js
@@ -73,7 +73,7 @@ define([
 
         reRender: function() {
             if (Adapt.device.screenSize != 'large') {
-                //this.replaceWithNarrative();
+                this.replaceWithNarrative();
             }
         },
 
